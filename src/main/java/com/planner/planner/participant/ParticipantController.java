9aa1d0e3164a4868faf11897c5ee90a4080ffad9
FelipeZelent <felipezelent@gmail.com>
2024-07-16 14:13:52 -0300

    v1

---
+++ src/main/java/com/planner/planner/participant/ParticipantController.java
@@ -0,0 +1,36 @@
+package com.planner.planner.participant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/participants")
+public class ParticipantController {
+
+    @Autowired
+    private ParticipantRepository repository;
+
+    @PostMapping("/{id}/confirm")
+    public ResponseEntity<Participant> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload) {
+        Optional<Participant> participant = this.repository.findById(id);
+        if (participant.isPresent()) {
+            Participant rawParticipant = participant.get();
+            rawParticipant.setConfirmed(true);
+            rawParticipant.setName(payload.name());
+
+            this.repository.save(rawParticipant);
+
+            return ResponseEntity.ok(rawParticipant);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+}
